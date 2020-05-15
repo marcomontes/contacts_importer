fa7ba73973afe99e30b8eda4f83bcd786e8d0835
Marco Montes <marcomontes@gmail.com>
2020-05-14 20:06:44 -0500

    validate all required data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < ApplicationRecord
   
   validates :name,  format: { with: NAME_REGEX, message: "only allows letters and -" } # Test Regex: https://rubular.com/r/o1xYlN995zu1DY
   validates :phone, format: { with: PHONE_REGEX, message: "has an invalid format. Must be (+00) 000 000 00 00 or  (+00) 000-000-00-00" } # Test Regex: https://rubular.com/r/uR58RiuL3mlwvD
-  validates :address, presence: true
+  validates :name, :birthdate_str, :birthdate_str, :phone, :address, :cc_franchise, :cc_number, :cc_last_four, :email, presence: true
   validates_with DateValidator, fields: [:birthdate_str]
 
   paginates_per 10
